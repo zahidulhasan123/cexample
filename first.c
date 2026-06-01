0d8b9d4bfe3d3661f3fd8449d2b7ba73255f1971
zahidulhasan123 <zahidul_hasan@cse.green.edu.bd>
2026-06-01 10:15:05 +0600

    basic

---
+++ first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    float cgpa=3.5;
+    int age=30;
+    char ch='A';
+    char name[]="Hasan";
+    double num=2323211;
+    
+    
+    printf("Name: %s  Age: %d  Grade: %c  CGPA: %.2f ID: %.2lf",name, age, ch, cgpa, num);
+    
+    return 0;
+}
